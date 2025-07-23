e3ebaa1f864fab6e35acfee78b656f908f173785
Felipe Castro <fpocastro@gmail.com>
2025-07-23 16:05:33 -0300

    Input correct disabling conditioning

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,9 +13,10 @@ export function Input({
   return (
     <input
       className={`
-        block w-full text-sm rounded-md border p-2.5 focus:outline-none focus:ring-2 focus:ring-green-500
+        block w-full text-sm rounded-md border p-2.5 bg-gray-50
+        focus:outline-none focus:ring-2 focus:ring-green-500 
+        disabled:bg-gray-100 disabled:cursor-not-allowed
         ${valid ? "border-gray-300" : "border-red-600"}
-        ${disabled ? "bg-gray-100 cursor-not-allowed" : "bg-gray-50"}
         ${className}
       `}
       disabled={disabled}
