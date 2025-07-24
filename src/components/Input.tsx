02a5af17caaef6b37adef0701117f98e6b008dc0
Felipe Castro <fpocastro@gmail.com>
2025-07-23 23:17:00 -0300

    Improved Input styling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,23 +4,16 @@ interface InputProps extends ComponentPropsWithoutRef<"input"> {
   valid?: boolean;
 }
 
-export function Input({
-  valid = true,
-  disabled = false,
-  className = "",
-  ...props
-}: InputProps) {
+export function Input({ valid = true, className = "", ...props }: InputProps) {
   return (
     <input
       className={`
-        block w-full text-sm rounded-md border p-2.5 bg-gray-50
+        relative block w-full h-10 text-sm rounded-sm border px-3 bg-gray-50
         focus:outline-none focus:ring-2 focus:ring-green-500 
-        disabled:bg-gray-100 disabled:cursor-not-allowed
+        disabled:cursor-not-allowed disabled:opacity-50
         ${valid ? "border-gray-300" : "border-red-600"}
         ${className}
       `}
-      disabled={disabled}
-      readOnly={disabled}
       aria-invalid={!valid}
       {...props}
     />
